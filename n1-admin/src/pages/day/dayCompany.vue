bc4f0e2844745c78ca7b161714d811070219cb17
HurryMan <1551920461@qq.com>
2019-03-27 12:32:00 +0800

    提交测试

--- n1-admin/src/pages/day/dayCompany.vue
+++ n1-admin/src/pages/day/dayCompany.vue
@@ -4,7 +4,6 @@
       <div class="top">
         <p class="title">
          <!--  当前用户列表--- -->
-         
           <RadioGroup v-model="source" class="radioGroup" type="button" @on-change="changeSource">
             <Radio label="0" v-if="permission.includes('正式数据')">正式</Radio>
             <Radio label="1">测试</Radio>
