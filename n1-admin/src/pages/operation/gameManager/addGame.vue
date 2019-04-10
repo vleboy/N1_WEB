35d6179de6975a06d289cb2d9caf96b531ee9753
HurryMan <1551920461@qq.com>
2019-04-10 17:27:35 +0800

    游戏管理 游戏类别本地化

--- n1-admin/src/pages/operation/gameManager/addGame.vue
+++ n1-admin/src/pages/operation/gameManager/addGame.vue
@@ -35,12 +35,8 @@
           <Button icon="ios-cloud-upload-outline" :loading="dialogLoading">请选择需要上传文件</Button>
         </Upload>
         <div style="padding: 16px 0">只能上传一张jpg/png文件，且不超过1M</div>
-        <div style="overflow: hidden"><img style="width: 80%" :src="managerInfo.gameImg"></div>
       </FormItem>
-      <FormItem label="网页游戏" style="text-align: left">
-        <Checkbox v-model="isShowWebGame"></Checkbox>
-      </FormItem>
-      <FormItem label="网页地址" v-if="isShowWebGame">
+      <FormItem label="网页地址">
         <Input v-model="managerInfo.gameLink" placeholder="请输入网页游戏地址"></Input>
       </FormItem>
     </Form>
@@ -132,6 +128,50 @@ export default {
       }
     } // 端口
     return {
+      GameListEnum: {
+        NA: [
+          { company: "NA", code: "70000", name: "H5电子游戏" },
+          { company: "NA", code: "90000", name: "H5电子游戏-无神秘奖" }
+        ],
+        TTG: [
+          { company: "TTG", code: "1010000", name: "TTG电子游戏" }
+        ],
+        PNG: [
+          { company: "PNG", code: "1020000", name: "PNG电子游戏" }
+        ],
+        MG: [
+          { company: "MG", code: "10300000", name: "MG电子游戏" }
+        ],
+        HABA: [
+          { company: "HABA", code: "1040000", name: "HABA电子游戏" }
+        ],
+        AG: [
+          { company: "AG", code: "1050000", name: "AG真人游戏" }
+        ],
+        SA: [
+          { company: "SA", code: "1060000", name: "SA真人游戏" },
+          { company: "SA", code: "1110000", name: "SA捕鱼游戏" }
+        ],
+        PG: [
+          { company: "PG", code: "1090000", name: "PG电子游戏" }
+        ],
+        YSB: [
+          { company: "YSB", code: "1130000", name: "YSB体育游戏" }
+        ],
+        RTG: [
+          { company: "RTG", code: "1140000", name: "RTG电子游戏" }
+        ],
+        SB: [
+          { company: "SB", code: "1080000", name: "SB电子游戏" },
+          { company: "SB", code: "1120000", name: "SB真人游戏" }
+        ],
+        DT: [
+          { company: "DT", code: "1150000", name: "DT电子游戏" }
+        ],
+        PP: [
+          { company: "PP", code: "1160000", name: "PP电子游戏" }
+        ]
+      },
       isfinish: {
         gameName: false,
         gameType: false,
@@ -280,12 +320,13 @@ export default {
       }
     },
     getGameType () {
-      !this.$store.state.add.isEdit && (this.managerInfo.gameType = '')
+      /* !this.$store.state.add.isEdit && (this.managerInfo.gameType = '')
       httpRequest("post", "/gameBigType", {
         companyIden: this.companyIden
       },'game').then(res => {
-        this.gameTypeOptions = res.payload
-      })
+        this.gameTypeOptions = res.payloads
+      }) */
+       this.gameTypeOptions = this.GameListEnum[this.companyIden]
     },
     getOperatorList () {
       httpRequest("post", "/companyList", {},'game').then(
